fde52a1725ce8345e3684473043821c5543bbfd6
unknown <rumelchow340@gmail.com>
2025-01-31 12:18:52 +0600

    SongLap app project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+ 
+  return (
+    <>
+     <h1 className="text-2xl text-[color:var(--primary-color)]">This is the SongLap Chat App</h1>
+   
+    </>
+  )
+}
+
+export default App
